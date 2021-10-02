82c68a960adcbcc560ae2831904622d72e399698
Marta Bartusch <martabartusch@gmail.com>
2021-10-02 18:27:55 +0200

    Cwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        String name = "Karol";
+        System.out.println("Dzień dobry!");
+        System.out.println("Dzień dobry " + name + "!");
+        System.out.printf("Biedna, mała %s chodzi do szkoły nr %d", "Hania", 12);
+        System.out.print("\nKoza ");
+        System.out.print("je ");
+        System.out.print("trawę.");
+
 
     }
 }
