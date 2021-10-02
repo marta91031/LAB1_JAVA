1f550275b52505fe24297a52f395fa8dfba518c0
Marta Bartusch <martabartusch@gmail.com>
2021-10-02 19:32:54 +0200

    Cwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,21 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        Scanner scan = new Scanner(System.in);
+        double [] tablica = new double [5];
+
+        for(int i = 0; i < tablica.length; i++)
+        {
+            System.out.printf("Podaj %d (liczba!)element tablicy\n", i+1);
+            tablica[i] = Double.parseDouble(scan.next());
+        }
+
+        System.out.println("Twoja tablica:");
+        for(int i = 0; i < tablica.length; i++)
+        {
+            System.out.println(tablica[i]);
+        }
+
 
     }
 }
