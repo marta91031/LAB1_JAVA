20dd68e81455d816c1bdff196f69d4ff112387df
Marta Bartusch <martabartusch@gmail.com>
2021-10-02 18:51:55 +0200

    Cwiczenie

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,15 @@ public class Main {
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
 
+        System.out.println("Zadanie 1");
+
+        String name = "Karolinka";
+        byte wiek = 8;
+        int ulLiczba = 5678;
+        double numer = 7839.8987654387329;
+        boolean like = true;
+        System.out.print(name + "\n" + wiek + "\n" + ulLiczba + "\n"+ numer + "\n" + like  + "\n");
+
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
         *    mnożenia, dziealenia i modulo na zmiennych:
@@ -14,17 +23,28 @@ public class Main {
         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
         *  */
 
-        String name = "Karolinka";
-        byte wiek = 8;
-        int ulLiczba = 5678;
-        double numer = 7839.8987654387329;
-        boolean like = true;
-        System.out.print(name + "\n" + wiek + "\n" + ulLiczba + "\n"+ numer + "\n"+ like);
+        System.out.println("Zadanie 2");
 
+        int liczbaA = 8;
+        int liczbaB = 5;
+        double liczbaX = 5.55;
+        double liczbaY = 8.25;
 
+        int suma = liczbaA + liczbaB;
+        int roznica = liczbaA - liczbaB;
+        int modulo = liczbaA%liczbaB;
 
+        System.out.println(suma);
+        System.out.println(roznica);
+        System.out.println(modulo);
 
+        double sumaD = liczbaX + liczbaY;
+        double roznicaD = liczbaX - liczbaY;
+        double moduloD = liczbaY%liczbaX;
 
+        System.out.println(sumaD);
+        System.out.println(roznicaD);
+        System.out.println(moduloD);
 
     }
 }
