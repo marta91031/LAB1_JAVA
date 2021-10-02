a9b1bcf784e4c3b9b02ca8f834fb6d87ee5784a4
Marta Bartusch <martabartusch@gmail.com>
2021-10-02 19:14:08 +0200

    Cwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,19 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true;
+        boolean b = true;
+        boolean c = false;
+        boolean d = false;
+        int e = 6;
+        int f = 8;
+        int g = 5;
+
+        System.out.println(a||b && !(a||b));
+        System.out.println((c||d) && (a&&b));
+        System.out.println(d==b);
+        System.out.println(a!=b&&b==d);
+        System.out.println((e>f)&&(g>f));
+
     }
 }
