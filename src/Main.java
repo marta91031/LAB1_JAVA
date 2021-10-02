0a3ee5e25e0a309717ee35e5083ff453d5764a0c
Marta Bartusch <martabartusch@gmail.com>
2021-10-02 19:00:34 +0200

    Cwiczenie 3

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,23 @@ public class Main {
         *   b) wyświetlić dane za pomocą println i printf
         */
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Podaj imię: ");
+        String name = scan.next();
+        System.out.print("Podaj nazwisko: ");
+        String lastName = scan.next();
+        System.out.print("Podaj wiek: ");
+        String age = scan.next();
+        System.out.print("Podaj nr indeksu: ");
+        String id = scan.next();
+
+        System.out.println("Imię: " + name);
+        System.out.println("Nazwisko: " + lastName);
+        System.out.println("Wiek: " + age);
+        System.out.println("Indeks: " + id);
+
+        System.out.printf("\nImię: %s\nNazwisko: %s\nWiek: %s\nIndeks: %s", name, lastName, age, id);
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
